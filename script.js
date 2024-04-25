0b83394a28cbed4d9bd19ed619978850c39cd3a6
Karolina Wadecka <wadeckakarolina@gmail.com>
2024-04-25 14:44:04 +0200

    exercises with arrays

---
+++ script.js
@@ -0,0 +1,9 @@
+const goalie = ["Sam"];
+const defenders = ["Lisa", "Tom", "Jenny", "Jo"];
+const midfielders = ["Jim", "Kai", "Ian", "Ava"];
+
+
+console.log("Goalie: "+ goalie);
+console.log("Defenders: " + defenders);
+console.log("Midfielders: " + midfielders);
+console.log("Forwards" + forwards);
